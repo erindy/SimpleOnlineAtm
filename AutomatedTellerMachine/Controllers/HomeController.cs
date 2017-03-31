bc7f5f03dfe9c47907c5af4427c8c84393c33c43
erindmehmeti <erindmehmeti@gmail.com>
2017-03-31 02:12:13 +0200

    SimpleOnlineAtmMVC

---
+++ AutomatedTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using AutomatedTellerMachine.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using AutomatedTellerMachine.Models;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var checkingAccountId = db.CheckingAccounts.Where(i => i.ApplicationUserId == userId).First().Id;
+            ViewBag.CheckingAccountId = checkingAccountId;
+            var manager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var user = manager.FindById(userId);
+            ViewBag.Pin = user.Pin;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Having trouble? Send us a message";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            ViewBag.TheMessage = "Thanks, we got your message!";
+
+            return PartialView("_ContactThanks");
+        }
+
+        public ActionResult Foo()
+        {
+            return View("About");
+        }
+
+        [AllowAnonymous]
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVCATM1";
+            if (letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            return Content(serial);
+        }
+    }
+}
---
+++ AutomatedTellerMachine/Controllers/TransactionController.cs
@@ -0,0 +1,57 @@
+﻿using AutomatedTellerMachine.Models;
+using AutomatedTellerMachine.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private IApplicationDbContext db;
+
+        public TransactionController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        public TransactionController(IApplicationDbContext dbContext)
+        {
+            db = dbContext;
+        }
+
+
+        // GET: Transaction/Deposit
+        public ActionResult Deposit(int checkingAccountId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Deposit(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Transactions.Add(transaction);
+                db.SaveChanges();
+                var service = new CheckingAccountService(db);
+                service.UpdateBalance(transaction.CheckingAccountId);
+                return RedirectToAction("Index", "Home");
+            }
+            
+            return View();
+        }
+
+
+        public ActionResult MyTransactions()
+        {
+            var userId = User.Identity.GetUserId();
+            var checkingAccountId = db.CheckingAccounts.Where(i => i.ApplicationUserId == userId).First().Id;
+            var TransactionList = db.Transactions.Where(t => t.CheckingAccountId == checkingAccountId);
+            return View(TransactionList.ToList());
+        }
+    }
+}
